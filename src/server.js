8c327de1e3fb29ec7dbe9e1765803aca6d70d910
Arkan Jbali <arkan.1997@gmail.com>
2019-01-24 03:09:27 +0200

    updates heroku 2

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + './dist/action'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/action/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, ()=>{console.log('Server Started');})

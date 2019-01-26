63ec5e020c15f7f32a6a5b9a9c8d0b63ac98cde0
Arkan Jbali <arkan.1997@gmail.com>
2019-01-26 19:36:11 +0200

    Check Heroku

--- src/server.js
+++ src/server.js
@@ -3,7 +3,20 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
 
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + './dist/action'));
 
@@ -13,4 +26,4 @@ res.sendFile(path.join(__dirname+'/dist/action/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, ()=>{console.log('Server Started');})
+app.listen(process.env.PORT || 5000);
